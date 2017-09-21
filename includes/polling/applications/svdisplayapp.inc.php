895c5deb40c08921e78fe5c2d67801237a722d9f
Marc Hassman <marc@scorevision.com>
2017-09-21 15:32:13 +0000

    updating for extra metrics

--- includes/polling/applications/svdisplayapp.inc.php
+++ includes/polling/applications/svdisplayapp.inc.php
@@ -12,18 +12,51 @@ $oid = '.1.3.6.1.4.1.8072.1.3.2.3';
 $rrd_name = array('app', $name, $app_id);
 $rrd_def = RrdDefinition::make()
 	->addDataset('mem', 'GAUGE')
-	->addDataset('cpu', 'GAUGE');
+	->addDataset('cpu', 'GAUGE')
+	->addDataset('6hour_crashes', 'GAUGE')
+	->addDataset('missing_heartbeat', 'GAUGE')
+        ->addDataset('heartbeat_lags', 'GAUGE')
+	->addDataset('dead_port', 'GAUGE');
 
 $resp = snmp_walk($device, $oid, $options, $mib);
 list($display_cpu, $display_memory) = explode("\n",$resp);
 
+function get_data($url) {
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+}
+
+$ip = gethostbyname($device["hostname"]);
+error_log('!!device is ' . $ip);
+
+$mini_status = get_data('https://app.scorevision.com/api/system_monitoring?ip_addr=' . $ip);
+error_log("JSON: " . $mini_status);
+#{"crashes":0,"double_disconnected_apps":0,"missing_heartbeat":true,"display_heartbeat_lags":false,"display_app_dead_port":false,"slow_response":0}
+if (strlen($mini_status) == 0) {
+	error_log("setting default return, string was empty");
+	$mini_status = "{\"crashes\":0,\"double_disconnected_apps\":0,\"missing_heartbeat\":true,\"display_heartbeat_lags\":false,\"display_app_dead_port\":false,\"slow_response\":0}";	
+} 
+$json_hash = json_decode($mini_status, true);
+
 $fields = array(
 	'display-app-mem' => (int)$display_memory,
 	'display-app-cpu' => (float)$display_cpu,
+	'display-app-6hour_crashes' => (int)$json_hash["crashes"],
+	'display-app-double_disconnected_apps' => (int)$json_hash["double_disconnected_apps"],
+	'display-app-missing_heartbeat' => $json_hash["missing_heartbeat"],
+	'display-app-heartbeat_lags' => (int)$json_hash["missing_heartbeat"],
+	'display-app-dead_port' => (int)$json_hash["display_app_dead_port"]
 );
 
+error_log("ARY: " . print_r(array_values($json_hash)));
 $tags = compact('name', 'app_id', 'rrd_name', 'rrd_def');
-if ($display_memory > 1900000000 || $display_cpu > .5) {
+if ($display_memory > 2100000000 || $display_cpu > .5) { # || (int)$json_hash["crashes"] > 0 || $json_hash["missing_heartbeat"]) {
   $status = "ERROR"; 
 } else {
   $status = "OK";
